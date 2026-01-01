fc554927c384508492ac65b2f7e287ed1f154b69
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2026-01-01 18:44:00 -0300

    chore: add shadcnui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
   return (
     <div>
-      <h1 className="bg-amber-400">Hello World</h1>
+      <Button>Hello</Button>
     </div>
   )
 }
